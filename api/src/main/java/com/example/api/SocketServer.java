c48373e0e9e9821878e739e51e028003c4981058
David Houle-Tymeczko <david.houletymeczko@ontariotechu.net>
2023-04-11 13:39:48 -0400

    stuff

--- api/src/main/java/com/example/api/SocketServer.java
+++ api/src/main/java/com/example/api/SocketServer.java
@@ -1,14 +1,21 @@
 package com.example.api;
 import jakarta.websocket.*;
+import jakarta.websocket.server.PathParam;
 import jakarta.websocket.server.ServerEndpoint;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
-@ServerEndpoint(value="/ws")
+@ServerEndpoint(value="/ws/{boardId}")
 public class SocketServer {
+    final private static Map<String, String> boardList = new HashMap<>();
+    final private static Map<String,String> roomHistoryList = new HashMap<>();
     @OnOpen
-    public void open(Session session) throws IOException {
+    public void open(@PathParam("boardId") String boardId, Session session) throws IOException {
+        boardList.put(session.getId(), boardId);
         session.getBasicRemote().sendText("{\"id\":\""+session.getId()+"\"}");
+
     }
 
     @OnClose
